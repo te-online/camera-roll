69737839dc487f3d040c8fa9249b72459fa59b33
Thomas Ebert <thomas.ebert@te-online.net>
2023-11-13 20:18:54 +0100

    Entities – Add public PhotoDto

--- CameraRollApi/Domain/Entities/Photo.cs
+++ CameraRollApi/Domain/Entities/Photo.cs
@@ -11,3 +11,13 @@ public class Photo
   public DateTime UpdatedAt { get; set; } = DateTime.Now;
   public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
+
+public class PhotoDto
+{
+  public Guid Id { get; set; } = Guid.NewGuid();
+  public string? Title { get; set; }
+  public string? Description { get; set; }
+  public string? S3Url { get; set; }
+  public DateTime UpdatedAt { get; set; } = DateTime.Now;
+  public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
