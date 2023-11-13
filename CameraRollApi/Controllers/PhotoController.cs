7fbf8fd0043ba470cc6d0a97d1190c43ff4cab73
Thomas Ebert <thomas.ebert@te-online.net>
2023-11-13 20:19:39 +0100

    API / Photos – Update controller to read from database and convert to Dto

--- CameraRollApi/Controllers/PhotoController.cs
+++ CameraRollApi/Controllers/PhotoController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CameraRollApi.Controllers;
 
@@ -8,29 +9,49 @@ namespace CameraRollApi.Controllers;
 public class PhotoController : ControllerBase
 {
     private readonly ILogger<PhotoController> _logger;
+    private readonly PhotoContext _context;
 
-    public PhotoController(ILogger<PhotoController> logger)
+    public PhotoController(ILogger<PhotoController> logger, PhotoContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     [HttpGet(
         "{photoId}",
         Name = "GetPhoto")
     ]
-    public Photo Get(Guid photoId)
+    public async Task<ActionResult<PhotoDto>> Get(Guid photoId)
     {
-        var photo = new Photo();
-        return photo;
+        var photo = await _context.Photos.FindAsync(photoId);
+
+        if (photo == null) {
+            return NotFound();
+        }
+
+        return ItemToDTO(photo);
     }
 
     [HttpGet(
         "Feed",
         Name = "GetPhotos"
     )]
-    public IEnumerable<Photo> GetPhotos()
+    public async Task<ActionResult<IEnumerable<PhotoDto>>> GetPhotos()
     {
-        Photo[] photos = { new() };
-        return photos;
+        return await _context.Photos
+            .Where(x => x.IsPublic == true)
+            .Select(x => ItemToDTO(x))
+            .ToListAsync();
     }
+
+    private static PhotoDto ItemToDTO(Photo photo) =>
+       new()
+       {
+           Id = photo.Id,
+           Description = photo.Description,
+           S3Url = photo.S3Url,
+           Title = photo.Title,
+           UpdatedAt = photo.UpdatedAt,
+           CreatedAt = photo.CreatedAt
+       };
 }
