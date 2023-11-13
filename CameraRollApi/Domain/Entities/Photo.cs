7080e05294df1ceb7dd3c6375a0e23ae657f00b3
Thomas Ebert <thomas@knoew.com>
2023-11-13 19:38:49 +0100

    Photo entity – Add defaults

--- CameraRollApi/Domain/Entities/Photo.cs
+++ CameraRollApi/Domain/Entities/Photo.cs
@@ -3,10 +3,11 @@ namespace Domain.Entities;
 public class Photo
 {
   public Guid Id { get; set; } = Guid.NewGuid();
+  public bool IsPublic { get; set; } = false;
   public string? Title { get; set; }
   public string? Description { get; set; }
   public string? S3Url { get; set; }
   public string? AuthorId { get; set; }
-  public DateTime UpdatedAt { get; set; }
-  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; } = DateTime.Now;
+  public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
