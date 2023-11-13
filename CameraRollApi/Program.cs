727543146070a966dffe617991b5620451794f5e
Thomas Ebert <thomas.ebert@te-online.net>
2023-11-13 20:18:39 +0100

    Storage – Configure InMemory Database

--- CameraRollApi/Program.cs
+++ CameraRollApi/Program.cs
@@ -1,3 +1,6 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Configure InMemory Database
+builder.Services.AddDbContext<PhotoContext>(opt => opt.UseInMemoryDatabase("Photos"));
 
 var app = builder.Build();
 
